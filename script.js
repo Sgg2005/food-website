2be7bf056df5e7a226bb2bf58cbc8f1b52a071f4
Simon <girmasimon95@gmail.com>
2025-06-19 20:46:48 +0400

    added menu bar to the site when it's responsive it will show up

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ document.querySelector('.login-btn').onclick = () =>
 {
   loginForm.classList.toggle('active');
 }
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>
+{
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
